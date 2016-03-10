bd3ba7e22113d0bd6e0c7c0f9769625622fe89b0
Oisin Prendiville <oisin@supertop.co>
2016-03-10 20:31:51 +0200

    Oops, adds the code

--- StackViewWow/ViewController.m
+++ StackViewWow/ViewController.m
@@ -10,18 +10,27 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *greenView;
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-  [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+- (IBAction)showTapped:(id)sender {
+  [UIView animateWithDuration:0.25 animations:^{
+    self.greenView.hidden = NO;
+    if (self.greenView.isHidden) {
+      NSLog(@":(");
+    } else {
+      NSLog(@":)");
+    }
+  }];
 }
 
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
+- (IBAction)hideTapped:(id)sender {
+  [UIView animateWithDuration:0.25 animations:^{
+    self.greenView.hidden = YES;
+  }];
 }
 
 @end
